b706d4780d7423ecc91f18ecf3b64b8537312881
Steven perry <Sperry3006@gmail.com>
2022-10-24 22:41:47 +0100

    Added a delete button, check boxes still not ehaving as expected

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import "./App.css";
 
 function App() {
   const [todos, setTodos] = useState([
-    { name: "Buy shopping", priority: "high" },
-    { name: "Clean bathroom", priority: "low" },
-    { name: "Car's MOT", priority: "high" },
+    { name: "Buy shopping", priority: true },
+    { name: "Clean bathroom", priority: true },
+    { name: "Car's MOT", priority: false },
   ]);
 
   const [newTodo, setNewTodo] = useState("");
+
+  const [isChecked, setChecked] = useState(true);
 
   const todoNodes = todos.map((todo, index) => {
     return (
       <li key={index} className={todo.priority ? "purchased" : "not-purchased"}>
         <span>{todo.name}</span>
         {todo.priority ? <span>high</span> : <span>low</span>}
+        {/* Trying to make a delete button */}
+        <button onClick={() => deleteTodo(index)}>
+          <span>X</span>
+        </button>
       </li>
     );
   });
@@ -23,13 +29,24 @@ function App() {
     setNewTodo(event.target.value);
   };
 
+  const handleChecked = (event) => {
+    console.log(event.target.value);
+    setChecked(event.target.value);
+  };
+
   const saveNewTodo = (event) => {
     event.preventDefault();
     const copyTodos = [...todos];
-    copyTodos.push({ name: newTodo, priority: "low" });
+    copyTodos.push({ name: newTodo, priority: isChecked });
     setTodos(copyTodos);
     setNewTodo("");
   };
+
+  function deleteTodo(index) {
+    const copyTodos = [...todos];
+    copyTodos.splice(index, 1);
+    setTodos(copyTodos);
+  }
 
   return (
     <div>
@@ -44,9 +61,21 @@ function App() {
           onChange={handleTodoInput}
         ></input>
         <label htmlFor="high">High</label>
-        <input type="radio" id="high" name="priority" value="high"></input>
+        <input
+          type="radio"
+          id="high"
+          name="priority"
+          value={isChecked}
+          onChange={handleChecked}
+        ></input>
         <label htmlFor="low">Low</label>
-        <input type="radio" id="low" name="priority" value="low"></input>
+        <input
+          type="radio"
+          id="low"
+          name="priority"
+          value={!isChecked}
+          onChange={handleChecked}
+        ></input>
         <input type="submit" value="Save Item"></input>
       </form>
 
