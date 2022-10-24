657fec0887e8edb34ce433eb7ea621ee41f1f67f
Steven perry <Sperry3006@gmail.com>
2022-10-24 18:54:11 +0100

    Completed MVP working on extensions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([
+    { name: "Buy shopping", priority: "high" },
+    { name: "Clean bathroom", priority: "low" },
+    { name: "Car's MOT", priority: "high" },
+  ]);
+
+  const [newTodo, setNewTodo] = useState("");
+
+  const todoNodes = todos.map((todo, index) => {
+    return (
+      <li key={index} className={todo.priority ? "purchased" : "not-purchased"}>
+        <span>{todo.name}</span>
+        {todo.priority ? <span>high</span> : <span>low</span>}
+      </li>
+    );
+  });
+
+  const handleTodoInput = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  const saveNewTodo = (event) => {
+    event.preventDefault();
+    const copyTodos = [...todos];
+    copyTodos.push({ name: newTodo, priority: "low" });
+    setTodos(copyTodos);
+    setNewTodo("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo's</h1>
+
+      <form onSubmit={saveNewTodo}>
+        {/* <label htmlFor="new-todo">Add a new todo item:</label> */}
+        <input
+          type="text"
+          id="new-todo"
+          value={newTodo}
+          onChange={handleTodoInput}
+        ></input>
+        <label htmlFor="high">High</label>
+        <input type="radio" id="high" name="priority" value="high"></input>
+        <label htmlFor="low">Low</label>
+        <input type="radio" id="low" name="priority" value="low"></input>
+        <input type="submit" value="Save Item"></input>
+      </form>
+
+      <ul>{todoNodes}</ul>
     </div>
   );
 }
